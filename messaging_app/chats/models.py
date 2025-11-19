f5b05944fd9f098a97413f0d0f1ea28c2f654d67
pius wanyangu <ndubipius96@gmail.com>
2025-11-19 22:37:20 +0300

    adding models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    # Replace integer PK with UUID
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
+    # fields already in abstractuser:
+    # firstname, lastname,email,password, username
+    phone_number = models.CharField(max_length=20, blank=True,null=True)
+    ROLE_CHOICES = [
+        ('guest','Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES,default='guest')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # make email unique (AbstractUser does not enforce this)
+    email = models.EmailField(unique=True)
+
+    REQUIRED_FIELDS=["email", "first_name","last_name"]
+    
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+
+
+class Conversation(models.Model):
+    Conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name="Conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"conversation {self.Conversation_id}"
+    
+# message model
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE,related_name="sent_messages") #links message to the user who sent it 
+    Conversation = models.ForeignKey(Conversation,on_delete=models.CASCADE,related_name="messages")
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message {self.message_id} from {self.sender}"
