dc09ee8ad4047ab9dc92b4508091e402af2c561a
pius wanyangu <ndubipius96@gmail.com>
2025-12-06 09:37:42 +0300

    parameterized unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import Mock, patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -47,3 +47,32 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+# parameterize and patch
+class TestMemoize(unittest.TestCase):
+    """Test cases for memoize"""
+
+    def test_memoize(self):
+        """Test that memoize caches the result of a_property"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        obj = TestClass()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            # Call memoized property twice
+            first = obj.a_property
+            second = obj.a_property
+
+            # Both results must match expected value
+            self.assertEqual(first, 42)
+            self.assertEqual(second, 42)
+
+            # Ensure underlying method was called exactly one time
+            mock_method.assert_called_once()
