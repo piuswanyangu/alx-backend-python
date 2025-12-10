ad651af00fc52f73f43bc37e60be948f30b72f06
pius wanyangu <ndubipius96@gmail.com>
2025-12-10 23:21:00 +0300

    restrivted access

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,11 +1,13 @@
-from datetime import datetime
+from datetime import datetime, time
 import logging
+
+from django.http import HttpResponseForbidden
 
 logger = logging.getLogger(__name__)
 
 class RequestLoggingMiddleware:
-    def __init__(self, get_respose):
-        self.get_response = get_respose
+    def __init__(self, get_response):
+        self.get_response = get_response
 
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else "Anonymous"
@@ -14,3 +16,19 @@ class RequestLoggingMiddleware:
         response = self.get_response(request)
         return response
     
+
+# restricted acces
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # allowed access from 6pm to 9pm
+        self.start_time = time(18,0)
+        self.end_time = time(21,0)
+
+    def __call__(self, request):
+        current_time = datetime.now().time()
+        # if the urrent time is out of  6-9pm deny acess
+        if not (self.start_time <= current_time <= self.end_time):
+            return HttpResponseForbidden("Acces to chat  is restricted at this time")
+        
+        return self.get_response(request)
--- Django-Middleware-0x03/chats/urls.py
+++ Django-Middleware-0x03/chats/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from messaging_app.chats import admin
 from .views import UserListView, ConversationListView, MessageListView
 
 router = DefaultRouter()
 router.register(r'messages', MessageListView, basename='message')
 
 urlpatterns = [
-     path('users/', UserListView.as_view()),
-     path('conversations/', ConversationListView.as_view()),
-     path('messages/', MessageListView.as_view()),
-     path('api', include(router.urls))
+     # path('users/', UserListView.as_view()),
+     # path('conversations/', ConversationListView.as_view()),
+     # path('messages/', MessageListView.as_view()),
+     # path('api', include(router.urls))
+     path('admin/', admin.site.urls),
 ]
